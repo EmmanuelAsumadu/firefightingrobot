0ed8c54923b6618ecfd24fee46c0a9e774fb55ca
technocopia <technocopia@technocopia-ThinkPad-T420s>
2023-01-14 15:30:46 -0500

    I didn't change anything I just added another cylinder
    I added a cylinder because I am trying to print an acoustic fire depresser

--- Opencylender.groovy
+++ Opencylender.groovy
@@ -2,3 +2,5 @@
 
 CSG sphere = new Sphere(25)// Spheres radius
 				.toCSG()// convert to CSG to display
+CSG simpleSyntax =new Cylinder(10,40).toCSG() // a one line Cylinder
+return [sphere,simpleSyntax]
