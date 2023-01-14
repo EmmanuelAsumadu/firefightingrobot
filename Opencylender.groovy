d9c3469eb5eb4eb8c26891c240836f18a092fb72
technocopia <technocopia@technocopia-ThinkPad-T420s>
2023-01-14 15:45:35 -0500

    I basically made a difference between the cylinder and the sphere
    For the acoustic depresser

--- Opencylender.groovy
+++ Opencylender.groovy
@@ -1,6 +1,14 @@
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cylinder
+import eu.mihosoft.vrl.v3d.Sphere
+
 //Your code here
 
 CSG sphere = new Sphere(25)// Spheres radius
 				.toCSG()// convert to CSG to display
 CSG simpleSyntax =new Cylinder(10,40).toCSG() // a one line Cylinder
-return [sphere,simpleSyntax]
+
+CSG newtihing = sphere.difference(simpleSyntax)
+
+
+return [newtihing]
