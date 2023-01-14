cc1bbe9b937367362591cd51776e1d903b50b7e1
technocopia <technocopia@technocopia-ThinkPad-T420s>
2023-01-14 16:15:46 -0500

    I made the chamber
    So I can execute the waves coming out of the speaker

--- Opencylender.groovy
+++ Opencylender.groovy
@@ -3,12 +3,16 @@ import eu.mihosoft.vrl.v3d.Cylinder
 import eu.mihosoft.vrl.v3d.Sphere
 
 //Your code here
+double Height_of_hamber = 140 
+double wallthickness = 5
 double spheresapce= 73.8/2
-CSG sphere = new Sphere(spheresapce*2)// Spheres radius
+CSG chamber_core =new Cylinder(spheresapce*2-wallthickness,Height_of_hamber-wallthickness*2).toCSG() // a one line Cylinder
+.movez(wallthickness)
+CSG banana  = new Cylinder(spheresapce*2,140)// Spheres radius
 				.toCSG()// convert to CSG to display
 CSG simpleSyntax =new Cylinder(spheresapce,150).toCSG() // a one line Cylinder
 
-CSG newtihing = sphere.difference(simpleSyntax)
-
+CSG newtihing = banana.difference(simpleSyntax)
+.difference(chamber_core)
 
 return [newtihing]
