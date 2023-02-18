fddda3bb622c3e2578f713deeadec3c7b9b682d9
technocopia <technocopia@technocopia-ThinkPad-T431s>
2023-02-18 14:20:05 -0500

    CALCULATE AIR PRESSURE.groovy

--- Opencylender.groovy
+++ Opencylender.groovy
@@ -3,12 +3,12 @@ import eu.mihosoft.vrl.v3d.Cylinder
 import eu.mihosoft.vrl.v3d.Sphere
 
 //Your code here
-double Height_of_hamber = 140 
+double Height_of_chamber = 140 
 double wallthickness = 5
 double spheresapce= 73.8/2
-CSG chamber_core =new Cylinder(spheresapce*2-wallthickness,Height_of_hamber-wallthickness*2).toCSG() // a one line Cylinder
+CSG chamber_core =new Cylinder(spheresapce*2-wallthickness,Height_of_chamber-wallthickness*2).toCSG() // a one line Cylinder
 .movez(wallthickness)
-CSG banana  = new Cylinder(spheresapce*2,140)// Spheres radius
+CSG banana  = new Cylinder(spheresapce*2.7,140)// Spheres radius
 				.toCSG()// convert to CSG to display
 CSG simpleSyntax =new Cylinder(spheresapce,150).toCSG() // a one line Cylinder
 
