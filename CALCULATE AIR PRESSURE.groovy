dea7da7db04e26a0a653077ec294e0e48c78745b
technocopia <technocopia@technocopia-ThinkPad-T431s>
2023-02-21 14:51:03 -0500

    sensors used for firefighting robot.groovy

--- CALCULATE AIR PRESSURE.groovy
+++ CALCULATE AIR PRESSURE.groovy
@@ -1,3 +1,20 @@
 // code here
-double diameterOfTheSpeaker = (8*25.4)
+double kilopacal= 101.325
+double diameterOfTheSpeaker = 8*25.4/1000.0
 println "The diameter of the speaker " + diameterOfTheSpeaker  
+double atmMin = 14.7/kilopacal
+double atmMax = 75/kilopacal
+double radiusOfTheSpeaker = diameterOfTheSpeaker/2
+double heightOfDifferential = 10.0/1000.0
+double volume = Math.PI*radiusOfTheSpeaker*radiusOfTheSpeaker*heightOfDifferential
+println "diameter of the speaker is " + diameterOfTheSpeaker
+println "the Minimum of atm is " + atmMin+"the Max of atm is " + atmMax
+println "radius of the speaker is " + radiusOfTheSpeaker
+println "height of differential is " + heightOfDifferential
+println "volume is " + volume
+double temp= 273 // standard temperature is always 273k
+double pressure=1.00 // standard pressure is always 1.00 atmosphere
+println "pressure " + pressure
+println "temp " + temp
+
+
