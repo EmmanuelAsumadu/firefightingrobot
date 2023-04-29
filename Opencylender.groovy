f36b194afba6980289d026873bcad6cb22a030c7
technocopia <technocopia@technocopia-ThinkPad-T431s>
2023-04-29 16:11:22 -0400

    Made a cylinder
    For the puffer

--- Opencylender.groovy
+++ Opencylender.groovy
@@ -4,9 +4,10 @@ import eu.mihosoft.vrl.v3d.Sphere
 
 CSG vitamin_capScrew_M3 = Vitamins.get("capScrew", "M3")
 CSG vitamin_heatedThreadedInsert_M3_pressIn = Vitamins.get("heatedThreadedInsert", "M3-pressIn")
-double heightOfChamber = 7*25.4
-double diameterOfChamber = 8.25*25.4
-CSG simpleSyntax = new Cylinder (diameterOfChamber,heightOfChamber) .toCSG()
+double heightOfChamber = 7*25.4/2 //now in inches 
+double radiusOfChamber = 8.25*25.4/2 //now in iches 
+CSG simpleSyntax = new Cylinder (radiusOfChamber,radiusOfChamber,heightOfChamber,(int)100).toCSG()
+
 						  
 //Your code here
 
